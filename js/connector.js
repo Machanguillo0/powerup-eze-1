c1a88d3c86596dd9c1069c3f0ca6cf544010d70c
Machanguillo0 <machanguillo@hotmail.com>
2026-06-13 20:28:57 +0100

    Campor tarjeta importes 15

--- js/connector.js
+++ js/connector.js
@@ -79,7 +79,7 @@ window.TrelloPowerUp.initialize({
       icon: './icon.svg', // Recomendado un icono gris, pero el SVG actual servirá
       content: {
         type: 'iframe',
-        url: t.signUrl('./section.html?v=52'), // Apuntamos a un nuevo archivo HTML que vamos a crear
+        url: t.signUrl('./section.html?v=53'), // Apuntamos a un nuevo archivo HTML que vamos a crear
         height: 250 // Altura inicial en píxeles (aumentado para evitar scrollbar)
       }
     };
