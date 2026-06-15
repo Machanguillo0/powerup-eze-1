05abe179425dc92f87535807f8190ef5a53a7a74
Machanguillo0 <machanguillo@hotmail.com>
2026-06-15 15:25:03 +0100

    Actualización campo a facturar

--- js/connector.js
+++ js/connector.js
@@ -79,7 +79,7 @@ window.TrelloPowerUp.initialize({
       icon: './icon.svg', // Recomendado un icono gris, pero el SVG actual servirá
       content: {
         type: 'iframe',
-        url: t.signUrl('./section.html?v=55'), // Apuntamos a un nuevo archivo HTML que vamos a crear
+        url: t.signUrl('./section.html?v=57'), // Apuntamos a un nuevo archivo HTML que vamos a crear
         height: 250 // Altura inicial en píxeles (aumentado para evitar scrollbar)
       }
     };
