2719822c05cf700a74a5b475210816922233f404
Machanguillo0 <machanguillo@hotmail.com>
2026-06-27 19:49:00 +0100

    nexo causal 15

--- js/connector.js
+++ js/connector.js
@@ -74,7 +74,7 @@ window.TrelloPowerUp.initialize({
   // Capability para añadir una sección grande dentro del cuerpo de la tarjeta
   'card-back-section': function (t, options) {
     return {
-      title: 'Powerup opciones 14',
+      title: 'Powerup opciones 15',
       icon: './icon.svg', // Recomendado un icono gris, pero el SVG actual servirá
       content: {
         type: 'iframe',
