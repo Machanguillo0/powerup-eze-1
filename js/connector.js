d9b0b2a9f047948be8f0faa52de277a8e4055406
Machanguillo0 <machanguillo@hotmail.com>
2026-07-08 12:06:12 +0100

    Facturas cliente cobra 4

--- js/connector.js
+++ js/connector.js
@@ -76,7 +76,7 @@ window.TrelloPowerUp.initialize({
   // Capability para añadir una sección grande dentro del cuerpo de la tarjeta
   'card-back-section': function (t, options) {
     return {
-      title: 'Powerup opciones 79',
+      title: 'Powerup opciones 80',
       icon: './icon.svg', // Recomendado un icono gris, pero el SVG actual servirá
       content: {
         type: 'iframe',
