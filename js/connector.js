00d938a0e4476c4c23dc41d8a94416198876b18d
Machanguillo0 <machanguillo@hotmail.com>
2026-07-16 20:00:19 +0100

    bug date 105

--- js/connector.js
+++ js/connector.js
@@ -76,11 +76,11 @@ window.TrelloPowerUp.initialize({
   // Capability para añadir una sección grande dentro del cuerpo de la tarjeta
   'card-back-section': function (t, options) {
     return {
-      title: 'Powerup opciones 104',
+      title: 'Powerup opciones 105',
       icon: './icon.svg', // Recomendado un icono gris, pero el SVG actual servirá
       content: {
         type: 'iframe',
-        url: t.signUrl('./section.html?v=101'), // Apuntamos a un nuevo archivo HTML que vamos a crear
+        url: t.signUrl('./section.html?v=' + Date.now()), // Evita el caché de la sección trasera de la tarjeta
         height: 250 // Altura inicial en píxeles (aumentado para evitar scrollbar)
       }
     };
