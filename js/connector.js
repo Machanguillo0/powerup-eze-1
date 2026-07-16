93601f1448d19eefb970edb4a2e7b68febc7d423
Machanguillo0 <machanguillo@hotmail.com>
2026-07-16 20:26:10 +0100

    bug date 107

--- js/connector.js
+++ js/connector.js
@@ -76,7 +76,7 @@ window.TrelloPowerUp.initialize({
   // Capability para añadir una sección grande dentro del cuerpo de la tarjeta
   'card-back-section': function (t, options) {
     return {
-      title: 'Powerup opciones 106',
+      title: 'Powerup opciones 107',
       icon: './icon.svg', // Recomendado un icono gris, pero el SVG actual servirá
       content: {
         type: 'iframe',
