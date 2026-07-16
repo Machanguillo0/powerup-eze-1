721dbb0ae81d97409636a8ef93cd9475ccab14aa
Machanguillo0 <machanguillo@hotmail.com>
2026-07-16 18:17:18 +0100

    cambios estéticos demanda causal

--- js/connector.js
+++ js/connector.js
@@ -76,7 +76,7 @@ window.TrelloPowerUp.initialize({
   // Capability para añadir una sección grande dentro del cuerpo de la tarjeta
   'card-back-section': function (t, options) {
     return {
-      title: 'Powerup opciones 99',
+      title: 'Powerup opciones 100',
       icon: './icon.svg', // Recomendado un icono gris, pero el SVG actual servirá
       content: {
         type: 'iframe',
