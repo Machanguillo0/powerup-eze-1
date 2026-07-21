19e18a4c0fdbfb33c648df12e8da206da9082083
Machanguillo0 <machanguillo@hotmail.com>
2026-07-21 15:27:46 +0100

    Control depurador proformas 1

--- js/connector.js
+++ js/connector.js
@@ -76,7 +76,7 @@ window.TrelloPowerUp.initialize({
   // Capability para añadir una sección grande dentro del cuerpo de la tarjeta
   'card-back-section': function (t, options) {
     return {
-      title: 'Powerup opciones 124',
+      title: 'Powerup opciones 126',
       icon: './icon.svg', // Recomendado un icono gris, pero el SVG actual servirá
       content: {
         type: 'iframe',
