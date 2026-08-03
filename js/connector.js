7d065395b2386081aaff258bc499f5d64b1350f6
Machanguillo0 <machanguillo@hotmail.com>
2026-08-03 11:42:58 +0100

    error correo

--- js/connector.js
+++ js/connector.js
@@ -76,7 +76,7 @@ window.TrelloPowerUp.initialize({
   // Capability para añadir una sección grande dentro del cuerpo de la tarjeta
   'card-back-section': function (t, options) {
     return {
-      title: 'Powerup opciones 2.62',
+      title: 'Powerup opciones 2.64',
       icon: './images/icon.svg', // Recomendado un icono gris, pero el SVG actual servirá
       content: {
         type: 'iframe',
