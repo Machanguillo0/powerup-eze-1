3d67f8062e7407a6a50f4808c6f323c3cf29b284
Machanguillo0 <machanguillo@hotmail.com>
2026-08-14 18:02:44 +0100

    fechas

--- js/connector.js
+++ js/connector.js
@@ -76,7 +76,7 @@ window.TrelloPowerUp.initialize({
   // Capability para añadir una sección grande dentro del cuerpo de la tarjeta
   'card-back-section': function (t, options) {
     return {
-      title: 'Powerup opciones 2.78',
+      title: 'Powerup opciones 2.79',
       icon: './images/icon.svg', // Recomendado un icono gris, pero el SVG actual servirá
       content: {
         type: 'iframe',
